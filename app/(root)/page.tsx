5d7bb68241a389446ae9e5d1e695591f6e5ff8df
vn578p0 <munisekhar.gurram@walmart.com>
2024-12-17 19:23:07 +0530

    Added home screen

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,30 @@
+
+
+import HeaderBox from '@/components/HeaderBox'
+import TotalBalanceBox from '@/components/TotalBalanceBox';
 import React from 'react'
 
 const Home = () => {
+    const loggedIn = {firstName : "Zhi"};
   return (
-    <div>Home</div>
+    <section className='home'>
+        <div className='home-content'>
+            <header className='home-header'>
+                <HeaderBox 
+                    type="greeting"
+                    title="Welcome"
+                    user={loggedIn?.firstName}
+                    subtext= "Access and Manage your Account and Transactions efficiently "
+                />
+
+                <TotalBalanceBox
+                    accounts={[]}
+                    totalBanks={1}
+                    totalCurrentBalance={1250.12}
+                />
+            </header>
+        </div>
+    </section>
   )
 }
 
