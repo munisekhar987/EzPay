1717fd22e174eb802901019b57bfbc534e744052
vn578p0 <munisekhar.gurram@walmart.com>
2024-12-27 17:16:26 +0530

    added , some more bars and payment tranform box

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -1,8 +1,36 @@
-import React from 'react'
-
-const MobileNav = ({user}:MobileNavProps) => {
+import {
+    Sheet,
+    SheetContent,
+    SheetDescription,
+    SheetHeader,
+    SheetTitle,
+    SheetTrigger,
+  } from "@/components/ui/sheet"
+import Image from "next/image"
+import Link from "next/link"
+  
+const MobileNav = ({ user }:MobileNavProps) => {
   return (
-    <div>MobileNav</div>
+    <section className="w-full max-w-[264px]">
+        <Sheet>
+            <SheetTrigger>
+                <Image
+                    src="/icons/hamburger.svg"
+                    width={30}
+                    height={30}
+                    alt="menu" 
+                    className="cursor-pointer"
+                />
+            </SheetTrigger>
+            <SheetContent side="left">
+                <SheetHeader>
+                    <Link>
+                    
+                    </Link>
+                </SheetHeader>
+            </SheetContent>
+        </Sheet>
+    </section>
   )
 }
 
