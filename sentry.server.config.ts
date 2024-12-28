e8d4f32fba8c77eb7f83909ba782891a55398759
vn578p0 <munisekhar.gurram@walmart.com>
2024-12-28 22:45:13 +0530

    added sentry and dwolla and plaid

---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://a61f6d7b94f97bcebc56c77030b265fd@o4508546509373440.ingest.de.sentry.io/4508546509832272",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
