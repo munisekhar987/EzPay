378c7a591ae4478dc1897b1f96b70c7c706a3a64
vn578p0 <munisekhar.gurram@walmart.com>
2025-01-06 22:03:15 +0530

    change in sentry

--- next.config.js
+++ next.config.js
@@ -1,5 +1,4 @@
-const { withSentryConfig } = require('@sentry/nextjs');
-
+import {withSentryConfig} from '@sentry/nextjs';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
